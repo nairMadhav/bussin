80470b3aec9a34c14197198272b6d2c15219acde
nairmadhav <maddy.nair01@gmail.com>
2021-08-20 18:59:17 +0530

    Added files to github for version control

---
+++ model/User/user.js
@@ -0,0 +1,8 @@
+const mongoose=require('mongoose');
+
+const userSchema=mongoose.Schema({
+        username:{type:String,required:true,unique:true},
+        password:{type:String,required:true}
+})
+const userModel=mongoose.model('userModel',userSchema);
+module.exports=userModel;
---
+++ server.js
@@ -0,0 +1,26 @@
+const express=require('express')
+const cors=require('cors')
+const mongoose=require('mongoose')
+const app=express();
+require('dotenv').config();
+
+//Middleware
+app.use(cors());
+//Mongoose config and connection
+const mongoURI=process.env.mongoURI;
+const mongoEssentials={
+    useCreateIndex:true,
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}
+mongoose.connect(mongoURI,mongoEssentials,(error)=>{
+    if(error){
+        console.log(error)
+    }
+    return(console.log('Connection to the database was successful'))
+})
+//server configuration
+const PORT=process.env.PORT || 5000
+app.listen(PORT,()=>{
+    console.log(`server is up on ${PORT}`)
+})
